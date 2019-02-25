17b3f6585cec515cc877886128ccc74fa295fbc9
Greetje Jongen <greetje.jongen@ucll.be>
2019-02-25 08:17:01 +0100

    Update DiceGUI.java

--- src/DiceGUI.java
+++ src/DiceGUI.java
@@ -2,7 +2,6 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
